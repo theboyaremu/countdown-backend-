e72d692cfca4f90c52abd25227d10005b4ddff29
Adefolajuwon <oadefolajuwon@gmail.com>
2024-10-19 11:51:17 +0100

    inital commit

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,11 +1,10 @@
 // routes/user.routes.js
 const express = require('express');
-const router = express.Router();
 const {createEvent, deleteEvent} = require("../controllers/event-controller.js")
 
-const userRouter = express.Router();
+const eventRouter = express.Router();
 
-userRouter.post('/sign-up', createEvent);
-userRouter.post('/sign-up', deleteEvent)
+eventRouter.post('/sign-up', createEvent);
+eventRouter.post('/sign-up', deleteEvent)
 
-module.exports = { userRouter };
+module.exports = { eventRouter };
